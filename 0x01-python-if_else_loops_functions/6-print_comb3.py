bcab3d2b5766e01405c54b8dab2890feda7d6762
Zainnyba <ayomiposijimoh@gmail.com>
2023-10-11 02:28:58 +0100

    oooh I loved this one

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(i, 10):
+        if i == j:
+            continue
+        if (i != 8):
+            print(f"{i}{j}", end=", ")
+        else:   
+            print(f"{i}{j}")
