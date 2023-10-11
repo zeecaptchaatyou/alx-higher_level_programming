37d7808f2bcd91ae70e75cf99aa50d9f68cd9348
Zainnyba <ayomiposijimoh@gmail.com>
2023-10-11 18:51:59 +0100

    fixed the string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -16,4 +16,4 @@ if last_digit > 5:
 elif last_digit == 0:
     print("and is 0")
 elif last_digit < 6 and last_digit != 0:
-    print("and is less than 6 and not zero")
+    print("and is less than 6 and not 0")
