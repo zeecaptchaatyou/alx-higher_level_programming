d8a53330d9a8b3709297c9c54e4005ff32f5cd4c
Zainnyba <ayomiposijimoh@gmail.com>
2023-10-11 03:13:59 +0100

    took me a few minute to figure out

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            char = (chr(ord(str[i]) - 32))
+        else:
+            char = str[i]
+        print(char, end="")
+    print("")
